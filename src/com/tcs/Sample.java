f0fb80453a45682318aa7dcad5688dcd555aa335
Vigneshvinoth <vigneshunicorn1@gmail.com>
2023-06-27 13:27:38 +0530

    new code

---
+++ src/com/tcs/Sample.java
@@ -0,0 +1,25 @@
+package com.tcs;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Sample {
+
+	
+
+	public static void main(String[] args) {
+
+		
+		String s="j1va2a3";
+		String sa = s.replaceAll("[a-z]","");
+		int parseInt = Integer.parseInt(sa);
+		System.out.println(10+parseInt);
+		
+		
+	
+
+			
+	}		
+	
+
+}
